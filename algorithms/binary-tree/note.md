7d61a2c0232893ba61bf478292aa0989ba72fb58
Olayinka Ibrahim <olayinka.ibrahim@etranzactng.com>
2024-01-29 12:38:27 +0100

    update notes

---
+++ algorithms/binary-tree/note.md
@@ -0,0 +1,7 @@
+## Binary Tree
+- A tree where every node has atmost two children 
+- exactly one root node
+- exactly 1 path between root and any node
+
+## Empty tree
+Tree with zero node
--- algorithms/sample.js
+++ algorithms/sample.js
@@ -1,5 +1,32 @@
 //
-function rename(arr, num) {}
+function insertionSort(arr) {
+    for(let i=1; i < arr.length; i++){
+        for(j=i; j > 0; j--){
+            if(arr[j] < arr[j-1]){
+                let temp = arr[j]
+                arr[j] = arr[j-1]
+                arr[j-1] = temp
+            }
+        }
+    }
+    return arr
+}
 
-const result = rename();
+function selectionSort(arr){
+    for(let i= 0; i < arr.length - 1; i++){
+        let minIndex = i
+        for(let j= i + 1; j < arr.length; j++){
+            if(arr[j] < arr[minIndex]){
+                minIndex = j
+            }
+        }
+        if(minIndex !== i){
+            [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
+        }
+    }
+
+    return arr
+}
+
+const result = selectionSort([2,5,10,1, 20, 0, 34, 45,12]);
 console.log(result);
